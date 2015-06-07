572fad682104771ead20ca087e63acce83c09e17
jssnokia520 <793525629@qq.com>
2015-06-07 21:46:43 +0800

    创建好工程,并设置好app图标和启动图片.

---
+++ 高仿微博/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  高仿微博
+//
+//  Created by JiShangsong on 15/6/7.
+//  Copyright (c) 2015年 JiShangsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSSAppDelegate class]));
+    }
+}
