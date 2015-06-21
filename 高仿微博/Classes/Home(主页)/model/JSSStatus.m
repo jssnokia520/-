07daae25fa1c5e0d0ea2da645abc59480140f368
jssnokia520 <793525629@qq.com>
2015-06-22 00:32:44 +0800

    完成时间的转换.

--- 高仿微博/Classes/Home(主页)/model/JSSStatus.m
+++ 高仿微博/Classes/Home(主页)/model/JSSStatus.m
@@ -17,4 +17,26 @@
     return @{@"pic_urls" : [JSSPhoto class]};
 }
 
+/**
+ *  拦截微博创建时间
+ */
+- (NSString *)created_at
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US"]];
+    [formatter setDateFormat:@"EEE MMM dd HH:mm:ss Z yyyy"];
+    NSDate *createDate = [formatter dateFromString:_created_at];
+    
+    NSLog(@"%@", createDate);
+    
+    NSDate *nowDate = [NSDate date];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSCalendarUnit calendarUnit = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSDateComponents *components = [calendar components:calendarUnit fromDate:createDate toDate:nowDate options:0];
+    NSLog(@"Year:%ld    Month:%ld   Day:%ld     Hour:%ld    Minute:%ld  Second:%ld", components.year, components.month, components.day, components.hour, components.minute, components.second);
+
+    return createDate.description;
+}
+
 @end
